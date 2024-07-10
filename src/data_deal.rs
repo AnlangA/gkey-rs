6919f22ba5604a16ee64d0dd4965a029398a7fdc
anlada <bo.hou@en-plus.com.cn>
2024-07-10 19:28:52 +0800

    1.修改生成toml文件时的bug

--- src/data_deal.rs
+++ src/data_deal.rs
@@ -16,14 +16,16 @@ pub async fn receive_app_data(mut rx: mpsc::Receiver<KeyInfo>) {
                             Err(e) => panic!("打开或创建文件key_info.toml 失败：{:?}", e),
     };
     let mut write = BufWriter::new(file);
-    let mut table_main = toml::value::Table::new();
     while let Some(message) = rx.recv().await {
+        let mut table_main = toml::value::Table::new();
         let mut table = toml::value::Table::new();
         table.insert("账户".to_string(), toml::Value::String(message.get_key_name()));
         table.insert("密码".to_string(), toml::Value::String(message.get_key()));
         table_main.insert(message.get_key_remark(), toml::Value::Table(table));
 
-        if let Ok(new_toml_str) = toml::to_string(&table_main){
+        if let Ok(mut new_toml_str) = toml::to_string(&table_main){
+            //换行
+            new_toml_str = new_toml_str + "\n";
             if let Err(e) = write.write(new_toml_str.as_bytes()).await{
                 eprintln!("{:?}",e);
             }
